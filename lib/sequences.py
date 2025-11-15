faa67f377cca81caa2efbe95a9f084ea3d211876
joshuaimbusi <joshua.imbusi@student.moringaschool.com>
2025-11-15 03:37:14 +0300

    "initial commit"

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+
+    fib = [0, 1]
+
+    # If the length is 1, only print [0]
+    if length == 1:
+        print([0])
+        return
+
+    # Build the sequence until it reaches the requested length
+    while len(fib) < length:
+        fib.append(fib[-1] + fib[-2])
+
+    print(fib)
